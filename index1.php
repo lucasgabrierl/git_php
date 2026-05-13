1a38b5a4311dabb7400e23c5b26dc17456623a1e
lucas gabriel <lucastracapapudinha@gmail.com>
2026-05-13 11:00:34 -0300

    15/04/2026

--- index1.php
+++ index1.php
@@ -1,10 +1,26 @@
 <?php
-$n1=7;
-$n2=8;
-$n3=7;
-$n4=6;
-
+$n1=readline("Digite a first nota: ");
+$n2=readline("Digite a segunda nota: ");
+$n3=readline("Digite a terceira nota: ");
+$n4=readline("Digite a quarta nota: ");
+$freq=readline("Digite a frequência: ");
 $med=($n1+$n2+$n3+$n4)/4;
 
-echo "a media  notas é:$med";
+
+if($freq<75){
+    echo "Reprovado por falta ";
+}else{
+    if($med>=6){
+        echo "Aprovado";
+    }else{
+        $notarec=readline("Digite a nota da recuperação: ");
+        $medfinal=($med+$notarec);
+        if($medfinal>5){
+            echo "Aprovado na recuperação";
+        }else{
+            echo "Reprovado, nos vemos ano que vem";
+        }
+    }
+}
+
 ?>
